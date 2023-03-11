978a7965b3272e22134a58b3923b270059835ece
David Schwartz <davidschwartz23@gmail.com>
2023-03-11 03:30:42 -0800

    Added mobile styling. Added mobile menu for sidebar and main menu. all sections now mobile friendly in html not react.

--- js/darkmode.js
+++ js/darkmode.js
@@ -3,3 +3,9 @@ const switchElement = document.querySelector('.switch')
 switchElement.addEventListener('click', () => {
   document.body.classList.toggle('dark')
 })
+
+const switchElement2 = document.querySelector('.switch2')
+
+switchElement2.addEventListener('click', () => {
+  document.body.classList.toggle('dark')
+})
